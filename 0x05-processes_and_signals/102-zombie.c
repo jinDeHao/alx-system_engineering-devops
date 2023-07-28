2d953d53c35836db8629d50af3e1ab3457b4e731
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-29 00:43:20 +0100

    creat 5 fake process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+/**
+ * infinite_while - an infinity loop
+ * Return: always 0
+*/
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - creats an 5 zombies process
+ * Return: always 0
+*/
+int main(void)
+{
+	int pid, i = 0;
+
+	while (i < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d", pid);
+			sleep(1);
+			i++;
+		}
+		else
+			return (0);
+	}
+	infinite_while();
+	return (0);
+}
