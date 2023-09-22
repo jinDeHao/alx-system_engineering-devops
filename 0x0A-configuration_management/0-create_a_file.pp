a5214c849092ff60408a9885f4e8e06b60f50560
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-23 00:11:05 +0100

    run school

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 file {
   '/tmp/school':
   ensure  => 'file',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I LOVE Puppet',
 }
