eab9e7124a06388cc961da59a10b93afcf39b468
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-22 23:50:31 +0100

    0 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #Create a file
-
 file {'/tmp/school':
 ensure  => 'file',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-content => 'I LOVE Puppet'
+content => 'I LOVE Puppet',
 }
