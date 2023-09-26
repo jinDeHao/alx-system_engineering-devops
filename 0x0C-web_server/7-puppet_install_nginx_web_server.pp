37487b0d1ba1d22358a67b1ded04efee75012d85
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-26 23:05:04 +0100

    puppet easier than bash

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+# install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+# website index file
+Nginx config
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# redirect_me config
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/home/omarnem/alx-system_engineering-devops/0x0C-web_server/file.me',
+  after  => 'server_name _;',
+  line   => '
+        location /redirect_me {
+            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+            # Am omar but you can call me dehao
+        }',
+}
+
+
+# run nginx
+exec { 'restart service':
+  command => 'service nginx start',
+  path    => ['/bin', '/usr/bin'],
+}
