2682ca3d4585d29620e032a39ab390b2efb3ee10
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-03 03:11:11 +0100

    pup

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
-# custom HTTP header with Puppet
+# custom header using puppet
 
 exec { 'update server':
   command  => 'sudo apt -y update',
   path     => ['/bin', '/usr/bin', '/usr/sbin'],
 }
-
+->
 exec { 'upgrade server':
   command  => 'sudo apt -y upgrade',
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
-
+->
 package { 'nginx':
   ensure   => installed,
 }
-
+->
 file_line { 'add custom header':
-  ensure  => present,
+  ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By $HOSTNAME;'
 }
-
+->
 exec { 'nginx':
   command  => 'sudo service nginx restart',
   path     => ['/bin', '/usr/bin', '/usr/sbin'],
