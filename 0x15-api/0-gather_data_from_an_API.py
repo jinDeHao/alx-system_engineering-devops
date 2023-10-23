97a5a11a3a324058a358f9e7231459d0d1c480af
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-23 23:44:15 +0100

    get the f***ing done tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+from sys import argv
+
+employee_ID = int(argv[1])
+person_res = requests.get("\
+https://jsonplaceholder.typicode.com/users/{}".format(employee_ID)).json()
+todo_list = requests.get("\
+https://jsonplaceholder.typicode.com/users/{}/todos\
+".format(employee_ID)).json()
+
+tasks = 0
+done = 0
+for i in todo_list:
+    if i["completed"]:
+        done += 1
+    tasks += 1
+
+print('Employee {} is done with tasks({}/{}):\
+'.format(person_res["name"],
+         done,
+         tasks))
+
+for i in todo_list:
+    if i["completed"]:
+        print("\t{}".format(i["title"]))
