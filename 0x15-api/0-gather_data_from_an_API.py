2b8782ba7e14ce69f2782b163d34172dbfe8b501
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-23 23:46:28 +0100

    get the f***ing done tasks[C

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,26 +3,26 @@
 
 import requests
 from sys import argv
+if __name__ != "__main__":
+    employee_ID = int(argv[1])
+    person_res = requests.get("\
+    https://jsonplaceholder.typicode.com/users/{}".format(employee_ID)).json()
+    todo_list = requests.get("\
+    https://jsonplaceholder.typicode.com/users/{}/todos\
+    ".format(employee_ID)).json()
 
-employee_ID = int(argv[1])
-person_res = requests.get("\
-https://jsonplaceholder.typicode.com/users/{}".format(employee_ID)).json()
-todo_list = requests.get("\
-https://jsonplaceholder.typicode.com/users/{}/todos\
-".format(employee_ID)).json()
+    tasks = 0
+    done = 0
+    for i in todo_list:
+        if i["completed"]:
+            done += 1
+        tasks += 1
 
-tasks = 0
-done = 0
-for i in todo_list:
-    if i["completed"]:
-        done += 1
-    tasks += 1
+    print('Employee {} is done with tasks({}/{}):\
+    '.format(person_res["name"],
+            done,
+            tasks))
 
-print('Employee {} is done with tasks({}/{}):\
-'.format(person_res["name"],
-         done,
-         tasks))
-
-for i in todo_list:
-    if i["completed"]:
-        print("\t{}".format(i["title"]))
+    for i in todo_list:
+        if i["completed"]:
+            print("\t{}".format(i["title"]))
