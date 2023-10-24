debb52882a04263ae924957b574e05f81b95ae23
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-24 02:21:28 +0100

    run 3 todos all

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    persons = requests.get("\
+https://jsonplaceholder.typicode.com/users/").json()
+
+    with open('todo_all_employees.json', 'w') as file:
+        for person in persons:
+            todo_list = requests.get("\
+https://jsonplaceholder.typicode.com/users/{}/todos\
+".format(person["id"])).json()
+            disctionary = {}
+            disctionary["{}".format(person["id"])] = []
+            user = disctionary["{}".format(person["id"])]
+            for i in todo_list:
+                user.append({"username": person["username"],
+                             "task": i["title"],
+                            "completed": i["completed"]})
+            json.dump(disctionary, file)
