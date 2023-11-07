8ef118ad6f0d3b53df086c3aa399e0dc3f4e2f0a
omarIdhmaid <o.idhmaid@gmail.com>
2023-11-07 21:12:20 +0100

    0 subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""reddit subscribers"""
+"""return subscribers"""
 import requests
 
 
@@ -8,7 +8,8 @@ def number_of_subscribers(subreddit):
     if not subreddit or not isinstance(subreddit, str):
         return 0
     r = requests.get(
-        'https://www.reddit.com/r/{}/about.json'.format(subreddit))
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers={'User-Agent': "omar"})
     if r.status_code == 200:
         data = r.json()["data"]
         return data["subscribers"]
