cfa682406517734dfbe42b8609c1936b0d3deafb
omarIdhmaid <o.idhmaid@gmail.com>
2023-11-09 01:25:11 +0100

    fix hex

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # match phpp replace by php
 exec { 'fix error':
-        command  => 'sed -i s/phpp/php/g file',
+        command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
         path     => ['/usr/bin', '/bin'],
 }
